09ab04711f80d1257c8b43c1e21f9601187f67de
Lucky Bea Soliven <45082129+LuckyBeaS@users.noreply.github.com>
2019-10-22 22:59:20 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\sluck                                            */
+/*    Created:      Tue Oct 22 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    3, 9            
+// LeadArmMotor         motor         1               
+// FollowArmMotor       motor         2               
+// ClawMotor            motor         10              
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
